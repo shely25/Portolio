ca9241b588829c7b6a36da959ef9b7795eab8f39
shely25 <shely.ece.25@gmail.com>
2022-06-10 23:15:58 +0600

    add about route

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-
+        <div className='text-white mt-5'>
+            <h1 className=' text-center '># About Me</h1>
+            <p className='m-5 p-5'>Myself Shakila Khatun Shely.I am an B.Sc Engineering Student at Khulna University of Engineering and Technogy.I was very much excited to know web developing and thats why I started a course and now I am confident to do work on front-end web deloping with knowledge and ability to build creative  and innovative web based application.I am hardworker,honest and enjoy working with team.</p>
         </div>
     );
 };
